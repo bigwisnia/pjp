f55a9c124a7a6ba80828691c277be958a28398e2
bigwisnia <kml.wski@gmail.com>
2016-01-19 23:15:06 +0100

    cw2_3mod

--- src/Cw2_3.groovy
+++ src/Cw2_3.groovy
@@ -2,7 +2,7 @@
  * Created by Ola on 2016-01-19.
  */
 
-def closure = {it > 2}
+def closure = {it * 2}
 def list = [1,2,3,4]
 
 List apply (List l, Closure c){
--- src/cw2_4.groovy
+++ src/cw2_4.groovy
@@ -18,19 +18,3 @@ while (input << showInputDialog('Podaj ciąg liczb', 'np: 1 2 3 4').tokenize(" "
 
 }
 
-CREATE TABLE RachunekSt(
-        IdRachunkuT int NOT NULL PRIMARY KEY,,
-        NumerRachunku char(26) NOT NULL,
-        DataZalozenia date NOT NULL,
-        IdWlascicielaT int NOT NULL,
-        ImieWlasciciela varchar(50) NOT NULL,
-        NazwiskoWlasciciela varchar(50) NOT NULL,
-        DataUrodzeniaWlasciciela date NOT NULL,
-        PlecWlasciciela char(1) NOT NULL,
-        IdMiastaWlascicielaT int NOT NULL,
-        DzielnicaWlasciciela varchar(100) NOT NULL,
-        MiastoWlasciciela varchar(255) NOT NULL,
-        WojewodztwoWlasciciela varchar(255) NOT NULL,
-        GrupaAktywnosci varchar(50) NULL
-)
-GO
