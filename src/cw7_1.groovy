a523d3d739f28d49f20722125f4d9bc70bd6b6d3
bigwisnia <kml.wski@gmail.com>
2016-01-20 23:22:21 +0100

    Zadanie 71

--- src/cw7_1.groovy
+++ src/cw7_1.groovy
@@ -1,5 +1,23 @@
 /**
  * Created by Ola on 2016-01-20.
  */
-String fileContents = new File('C:\\Users\\Ola\\Desktop\\pjp\\texts\\Projekty.txt').getText('UTF-8')
-println fileContents
+File file = new File('C:\\Users\\Ola\\Desktop\\pjp\\texts\\Projekty.txt')
+def projectMap = [:]
+
+file.eachLine {l ->
+
+
+
+    Scanner s = new Scanner(l).useDelimiter("\\t");
+    String tempp = s.next()
+    def tempList = []
+    while (s.hasNext()){
+      tempList << s.next()
+    }
+    println tempList
+
+    projectMap["$tempp"] = tempList
+    s.close()
+}
+String fileContents = file.getText('UTF-8')
+println projectMap
