d2be46a3d0d4450caf02ec92096d5acca34640e2
bigwisnia <kml.wski@gmail.com>
2016-01-21 01:49:09 +0100

    Zadanie 76_3 cos lipa

--- src/cw6_3.groovy
+++ src/cw6_3.groovy
@@ -1,12 +1,10 @@
+
+
 /**
  * Created by Ola on 2016-01-21.
  */
 File filePl = new File('C:\\Users\\Ola\\Desktop\\pjp\\texts\\dates.txt')
 String fileContents = filePl.getText('UTF-8')
-
-
-
-
 
 def regex = /\d{4}\-\d{2}\-\d{2}/
 def matcher = fileContents =~ regex
@@ -16,18 +14,17 @@ matcher.each { res << it }
 println res
 
 res.each {e ->
-    date = new Date().parse('yyyy-MM-dd', "$e" )
-    println date
-    Calendar cal = Calendar.getInstance();
-    cal.setLenient(false);
-    cal.setTime();
-    try {
-        cal.getTime();
+    def dateParser = new java.text.SimpleDateFormat("yyy-MM-dd")
+    dateParser.lenient = false
+    try{
+        def date = dateParser.parse("$e")
+        println date
     }
-    catch (Exception em) {
-        System.out.println("Invalid date");
+    catch (Exception ex){
+        println "INVALID DATE" + ' ' + "$e"
     }
 
 
 }
 
+// Data jesli miesiac jest wiekszy od 31 odejmuje od tej daty np
